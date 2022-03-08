ad39aa6607d6b3e194f38a28d145268e9c21b795
ruchisharma@dcmail.ca <ruchisharma@dcmail.ca>
2022-03-08 05:33:58 -0500

    Allow block editor to edit single product

--- themes/dice/inc/woocommerce.php
+++ themes/dice/inc/woocommerce.php
@@ -5,10 +5,14 @@
  * @package Roll_Your_Dice
  */
 
+ /**
+  * Allow block editor for single products
+  */
+
 function dice_use_block_editor_for_post_type( $use_block_editor, $post_type ) {
     if ( 'product' === $post_type ) {
         $use_block_editor = true;
     }
     return $use_block_editor;
 }
- add_filter( 'use_block_editor_for_post_type', 'dice_use_block_editor_for_post_type', 10,2 )
+ add_filter( 'use_block_editor_for_post_type', 'dice_use_block_editor_for_post_type', 10,2 );
