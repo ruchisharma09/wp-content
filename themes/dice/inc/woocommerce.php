b7e714b7a8bcda87a72e41e0e16a5539c2fc6df1
ruchisharma@dcmail.ca <ruchisharma@dcmail.ca>
2022-03-08 04:55:09 -0500

    Create a function dice_use_block_editor_for_post_type
    Create this function dice_use_block_editor_for_post_type( $use_block_editor, $post_type )  so that we can use block editor to edit products.

--- themes/dice/inc/woocommerce.php
+++ themes/dice/inc/woocommerce.php
@@ -5,4 +5,10 @@
  * @package Roll_Your_Dice
  */
 
+function dice_use_block_editor_for_post_type( $use_block_editor, $post_type ) {
+    if ( 'product' === $post_type ) {
+        $use_block_editor = true;
+    }
+    return $use_block_editor;
+}
  add_filter( 'use_block_editor_for_post_type', 'dice_use_block_editor_for_post_type', 10,2 )
