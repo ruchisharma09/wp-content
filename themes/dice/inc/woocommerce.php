0cc95da22a75dfdaa132c71279af2ec4360ff33e
ruchisharma@dcmail.ca <ruchisharma@dcmail.ca>
2022-03-08 05:44:41 -0500

    Add title to single Product add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 4 );

--- themes/dice/inc/woocommerce.php
+++ themes/dice/inc/woocommerce.php
@@ -16,3 +16,8 @@ function dice_use_block_editor_for_post_type( $use_block_editor, $post_type ) {
     return $use_block_editor;
 }
  add_filter( 'use_block_editor_for_post_type', 'dice_use_block_editor_for_post_type', 10,2 );
+ 
+ /**
+  * Re-add product title
+  */
+add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 4 );
